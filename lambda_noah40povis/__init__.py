f3a73354190dbbd0baa83bbafc06efc229b30e26
noah40povis <n.c.povis@gmail.com>
2020-07-08 20:24:20 -0400

    yerp

--- lambda_noah40povis/__init__.py
+++ lambda_noah40povis/__init__.py
@@ -5,11 +5,10 @@ lambdata - a collection of Data Science helper functions
 
 import pandas as pd
 import numpy as np
-from . import example_module
 from sklearn.model_selection import train_test_split
 
 TEST = pd.DataFrame(np.ones(10))
-Y = example_module.increment(example_module.x)
+
 
 def null_info(x):
     a = x.isnull().sum()
